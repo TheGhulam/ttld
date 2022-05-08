19e8c5c0669bef2e7436e3f5dbd9a6a4bac10bf5
EfeKN <efe.kaan.fidanci@gmail.com>
2022-05-08 11:33:03 +0300

    endscreen class

---
+++ TTLD/core/src/screens/EndScreen.java
@@ -0,0 +1,44 @@
+package screens;
+
+import com.badlogic.gdx.Screen;
+import com.ttld.game.ttld;
+
+public class EndScreen implements Screen {
+    public EndScreen(ttld ttldGame) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
